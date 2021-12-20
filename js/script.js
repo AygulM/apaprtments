6dc088dbf1975b8c4252a5b6f0b780ecca86ff7c
unknown <aygulmukhamedyarova01@gmail.com>
2021-12-21 01:00:44 +0500

    ver4

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 let btnPrev = document.getElementById("apartmentOneLeft");
 let bnNext = document.getElementById("apartmentOneRight");
 let img = document.getElementById("apartmentOneSlider");
-let slider = ["img/apartments__img/a1/1.jpg", 
-"img/apartments__img/a1/2.jpg", 
-"img/apartments__img/a1/3.jpg", 
-"img/apartments__img/a1/4.jpg", 
-"img/apartments__img/a1/5.jpg"];
+let slider = ["img/apartments__img/a2/1.jpg", 
+"img/apartments__img/a2/2.jpg", 
+"img/apartments__img/a2/3.jpg", 
+"img/apartments__img/a2/4.jpg"];
 let current = 0 ;
 bnNext.addEventListener("click", moveSlideNext);
 btnPrev.addEventListener("click", moveSlidePrev)
