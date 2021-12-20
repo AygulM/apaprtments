c19ac54ff3498caefe3df6c619f8d2480611b60e
unknown <aygulmukhamedyarova01@gmail.com>
2021-12-20 14:41:34 +0500

    ver04

--- js/slider1.js
+++ js/slider1.js
@@ -2,6 +2,8 @@ let nextBtn = document.getElementById("NextBtn");
 let prevBtn = document.getElementById("PrevBtn");
 let items = document.querySelector(".catalog__items");
 let curren=0;
+let fillNext = document.querySelector(".cbtn__next");
+let fillPrev = document.querySelector(".cbtn__prev");
 items.style.left= String(curren)+"%";
 nextBtn.addEventListener("click", ()=>{
     curren -= 34;
@@ -9,14 +11,17 @@ nextBtn.addEventListener("click", ()=>{
     if (items.style.left <= -102+"%")
     {
         nextBtn.setAttribute('disabled', true);
+        fillNext.style.fill = "lightgrey";
     } 
     if(items.style.left < 0+'%'){
         prevBtn.removeAttribute('disabled');
+        fillPrev.style.fill = "#6E9773";
     }
 })
 prevBtn.addEventListener("click", ()=>{
     if(items.style.left>=0+'%'){
         prevBtn.setAttribute('disabled', true);
+        fillPrev.style.fill = "lightgrey"
     }
     else{
         curren += 34;
@@ -24,5 +29,6 @@ prevBtn.addEventListener("click", ()=>{
     }
     if(items.style.left > -102+"%"){
         nextBtn.removeAttribute('disabled');
+        fillNext.style.fill = "#6E9773"
     }
 })
